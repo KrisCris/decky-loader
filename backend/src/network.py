da0ee9c86a73b3a1d1d879126d81b5d0441d5160
connlost <4348524953.h@gmail.com>
2024-06-14 12:30:52 +0800

    Proxy: check ip:port availability before making any network test

--- backend/src/network.py
+++ backend/src/network.py
@@ -1,4 +1,5 @@
 import ipaddress
+from logging import getLogger
 import socket
 from urllib.parse import urlparse
 from aiohttp import BaseConnector, ClientSession, ClientResponse, TCPConnector
@@ -6,6 +7,8 @@ from aiohttp.typedefs import StrOrURL
 from yarl import URL
 from aiohttp_socks import ProxyConnector, ProxyType # type: ignore
 from typing import Any, Optional
+
+logger = getLogger("ProxiedClient")
 
 class ProxiedClientSession(ClientSession):
     bypass_local: bool = True
@@ -16,6 +19,27 @@ class ProxiedClientSession(ClientSession):
 
     async def _try_get_proxy_connector(self) -> Optional[ProxyConnector]:
         if self.proxy_url:
+            parsed_url = urlparse(self.proxy_url)
+            if parsed_url.scheme not in ["http", "https", "socks4", "socks5"]:
+                logger.warning(f"Unsupported proxy scheme: {parsed_url.scheme}")
+                return None
+            
+            ip = parsed_url.hostname
+            port = parsed_url.port
+            if not ip or not port:
+                logger.warning(f"Invalid proxy URL: {self.proxy_url}")
+                return None
+            
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.settimeout(1)
+            try: 
+                sock.connect((ip, port))
+            except:
+                logger.warning(f'Proxy {self.proxy_url} is not available') 
+                return None
+            finally: 
+                sock.close()
+
             connector = ProxyConnector.from_url(self.proxy_url) # type: ignore
             try:
                 async with ClientSession(connector=connector) as session:
@@ -47,6 +71,8 @@ class ProxiedClientSession(ClientSession):
             self._connector = self.default_connector
         else:
             self._connector = await self._try_get_proxy_connector() or self.default_connector
+            
+        logger.info(f"Using {'Proxied' if self._connector != self.default_connector else 'Direct'} connector for {method} request: {str_or_url}")
         return await super()._request(method, str_or_url, *args, **kwargs)         
 
     def __init__(self, *args: Any, **kwargs: Any):
